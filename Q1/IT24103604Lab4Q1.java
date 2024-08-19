dde5510cc38d4c89d65efdc14cfd1417bc976556
IT24103604 <it24103604@my.sliit.lk>
2024-08-19 23:14:14 +0530

    Add files via upload

---
+++ Q1/IT24103604Lab4Q1.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+public class IT24103604Lab4Q1 {
+	public static void main(String[]args){
+		Scanner scan = new Scanner(System.in);
+		
+		System.out.print("Enter a number: ");
+		int num = scan.nextInt();
+		
+		if(num > 0){
+			System.out.println("The number is: Positive");
+		}
+		else if(num < 0) {
+			System.out.println("The number is: Negative");
+		}
+		else{
+			System.out.println("The number is Zero");
+		}
+	}
+}  
